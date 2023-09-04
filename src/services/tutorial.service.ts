9a09bbb7472ec5e9ea2fd098e289e39b6888a521
amrita-jaiswal <amritajais10@gmail.com>
2023-09-04 16:14:53 +0530

    updated service

--- src/services/tutorial.service.ts
+++ src/services/tutorial.service.ts
@@ -28,14 +28,14 @@ import { initializeApp } from "firebase/app";
 // export default TutorialDataService;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVXHZbjyHExp9QcWdoLE_Ma6Hd7MQ6WmY",
+  apiKey: "abcd",
   authDomain: "crud-fa54c.firebaseapp.com",
   projectId: "crud-fa54c",
   storageBucket: "crud-fa54c.appspot.com",
-  messagingSenderId: "202760801584",
-  appId: "1:202760801584:web:3228f4b34827343759ccf5",
-  measurementId: "G-ZK8TXLK6E9",
-  databaseURL: "https://crud-fa54c-default-rtdb.firebaseio.com",
+  messagingSenderId: "abcd",
+  appId: "abcd",
+  measurementId: "abcd",
+  databaseURL: "abcd",
 };
 
 // Initialize Firebase
